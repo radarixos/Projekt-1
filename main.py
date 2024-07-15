4340191c4a9a0ae9659edf74568871f054675c07
Radarixos <47194382+radarixos@users.noreply.github.com>
2024-07-15 18:58:25 +0200

    Create main.py
    first attempt...

---
+++ main.py
@@ -0,0 +1,113 @@
+"""
+
+projekt_1.py: první projekt do Engeto Online Python Akademie
+
+author: Radek Svejda
+
+email: ok1vbr@gmail.com
+
+discord: radarixos
+
+"""
+#godblessai4help
+
+import sys
+
+# Users list
+users = {
+    "bob": "123",
+    "ann": "pass123",
+    "mike": "password123",
+    "liz": "pass123"
+}
+
+# Texts from ext source
+TEXTS = ['''
+Situated about 10 miles west of Kemmerer,
+Fossil Butte is a ruggedly impressive
+topographic feature that rises sharply
+some 1000 feet above Twin Creek Valley
+to an elevation of more than 7500 feet
+above sea level. The butte is located just
+north of US 30N and the Union Pacific Railroad,
+which traverse the valley. ''',
+'''At the base of Fossil Butte are the bright
+red, purple, yellow and gray beds of the Wasatch
+Formation. Eroded portions of these horizontal
+beds slope gradually upward from the valley floor
+and steepen abruptly. Overlying them and extending
+to the top of the butte are the much steeper
+buff-to-white beds of the Green River Formation,
+which are about 300 feet thick.''',
+'''The monument contains 8198 acres and protects
+a portion of the largest deposit of freshwater fish
+fossils in the world. The richest fossil fish deposits
+are found in multiple limestone layers, which lie some
+100 feet below the top of the butte. The fossils
+represent several varieties of perch, as well as
+other freshwater genera and herring similar to those
+in modern oceans. Other fish such as paddlefish,
+garpike and stingray are also present.'''
+]
+#
+def authenticate(username, password):
+    return users.get(username) == password
+
+def get_text_choice():
+    try:
+        choice = int(input("Enter a number btw. 1 and 3 to select: "))
+        if choice not in [1, 2, 3]:
+            print("Invalid choice, terminating the program...")
+            sys.exit()
+        return TEXTS[choice - 1]
+    except ValueError:
+        print("Invalid input, terminating the program...")
+        sys.exit()
+
+def analyze_text(text):
+    words = text.split()
+    num_words = len(words)
+    titlecase_words = sum(1 for word in words if word.istitle())
+    uppercase_words = sum(1 for word in words if word.isupper())
+    lowercase_words = sum(1 for word in words if word.islower())
+    numeric_strings = sum(1 for word in words if word.isdigit())
+    sum_of_numbers = sum(int(word) for word in words if word.isdigit())
+
+    print(f"There are {num_words} words in the selected text.")
+    print(f"There are {titlecase_words} titlecase words.")
+    print(f"There are {uppercase_words} uppercase words.")
+    print(f"There are {lowercase_words} lowercase words.")
+    print(f"There are {numeric_strings} numeric strings.")
+    print(f"The sum of all the numbers {sum_of_numbers}")
+    print("----------------------------------------")
+
+    word_lengths = {}
+    for word in words:
+        word_length = len(word)
+        if word_length in word_lengths:
+            word_lengths[word_length] += 1
+        else:
+            word_lengths[word_length] = 1
+
+    print("LEN|  OCCURENCES  |NR.")
+    print("----------------------------------------")
+    for length in sorted(word_lengths):
+        print(f"{length:3}|{'*' * word_lengths[length]:<13}|{word_lengths[length]}")
+
+def main():
+    username = input("username: ")
+    password = input("password: ")
+
+    if authenticate(username, password):
+        print("----------------------------------------")
+        print(f"Welcome to the app, {username}")
+        print("We have 3 texts to be analyzed.")
+        print("----------------------------------------")
+        text = get_text_choice()
+        print("----------------------------------------")
+        analyze_text(text)
+    else:
+        print("unregistered user, terminating the program..")
+
+if __name__ == "__main__":
+    main()
