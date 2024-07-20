ff16fed74a2d5a7b7f6a0c028b5c2c76739ab9a1
Radarixos <47194382+radarixos@users.noreply.github.com>
2024-07-20 22:34:45 +0200

    Update main.py
    oprava vertikalniho zarovnani polozek sloupcoveho grafu

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ email: ok1vbr@gmail.com
 discord: radarixos
 
 """
-# rev0.3rc
-# godblessai4help
+# ver 0.4
+# tnxai!
 
 import sys
 import string
@@ -34,22 +34,22 @@ above sea level. The butte is located just
 north of US 30N and the Union Pacific Railroad,
 which traverse the valley. ''',
          '''At the base of Fossil Butte are the bright
-red, purple, yellow and gray beds of the Wasatch
-Formation. Eroded portions of these horizontal
-beds slope gradually upward from the valley floor
-and steepen abruptly. Overlying them and extending
-to the top of the butte are the much steeper
-buff-to-white beds of the Green River Formation,
-which are about 300 feet thick.''',
+         red, purple, yellow and gray beds of the Wasatch
+         Formation. Eroded portions of these horizontal
+         beds slope gradually upward from the valley floor
+         and steepen abruptly. Overlying them and extending
+         to the top of the butte are the much steeper
+         buff-to-white beds of the Green River Formation,
+         which are about 300 feet thick.''',
          '''The monument contains 8198 acres and protects
-a portion of the largest deposit of freshwater fish
-fossils in the world. The richest fossil fish deposits
-are found in multiple limestone layers, which lie some
-100 feet below the top of the butte. The fossils
-represent several varieties of perch, as well as
-other freshwater genera and herring similar to those
-in modern oceans. Other fish such as paddlefish,
-garpike and stingray are also present.'''
+         a portion of the largest deposit of freshwater fish
+         fossils in the world. The richest fossil fish deposits
+         are found in multiple limestone layers, which lie some
+         100 feet below the top of the butte. The fossils
+         represent several varieties of perch, as well as
+         other freshwater genera and herring similar to those
+         in modern oceans. Other fish such as paddlefish,
+         garpike and stingray are also present.'''
          ]
 
 
@@ -100,10 +100,10 @@ def analyze_text(text):
         else:
             word_lengths[word_length] = 1
 
-    print("LEN|  OCCURRENCES  |NR.")
+    print("LEN|    OCCURRENCES     |NR.")
     print("----------------------------------------")
     for length in sorted(word_lengths):
-        print(f"{length:3}|{'*' * word_lengths[length]:<13}|{word_lengths[length]}")
+        print(f"{length:3}| {'*' * word_lengths[length]:<18} |{word_lengths[length]}")
 
 
 def main():
